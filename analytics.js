b2926633afe02761e2c40dc369de95f669e58172
philokane <phil@icedcoffee.ie>
2026-04-23 01:32:08 +0100

    add analytics.js

---
+++ analytics.js
@@ -0,0 +1,12 @@
+var sc_project=13159031; 
+var sc_invisible=1; 
+var sc_security="b14eeb4d"; 
+
+(function() {
+  var sc = document.createElement('script');
+  sc.type = 'text/javascript';
+  sc.async = true;
+  sc.src = 'https://www.statcounter.com/counter/counter.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(sc, s);
+})();
